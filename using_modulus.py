6b888472b51d445264d34c7e6276f961a261d34f
waithakan <waithakadylan@gmail.com>
2023-01-30 14:37:08 +0300

    new work

---
+++ using_modulus.py
@@ -0,0 +1,21 @@
+#using modulus to  print even and odd numbers
+#even number
+number = int(input("Please enter an even number: "))
+if (number%2 == 0):
+    print(number, "is an even number")
+else:
+    print(number, "is not an even number")
+
+#odd number
+number = int(input("Please enter an odd number: "))
+if (number%3 == 0):
+    print(number, "is an odd number")
+else:
+    print(number, "is not an odd number")
+
+#divisibility test of 5
+number = int(input("Please enter a multiple of 5 number: "))
+if (number%5 == 0):
+    print(number, "is divisible by 5")
+else:
+    print(number, "is not divisible by 5")
